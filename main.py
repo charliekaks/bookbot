2aa47100f4ea52839d7f12f9bc2979f67a6d114f
charliekaks <kaksunchained@gmail.com>
2024-05-19 22:15:27 +0300

    first iteration without without converting to lower case

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    get_character_count(text)
 
 
 def get_num_words(text):
@@ -14,6 +15,20 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+
+character_dict = {}
+def get_character_count(text):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for character in alphabet:
+        count = 0
+        for char in text:
+            if(char == character):
+                count+=1
+                character_dict[character] = count
+                print(count)
+    print(character_dict)
+
 
 
 main()
